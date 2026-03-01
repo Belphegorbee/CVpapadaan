3470ee59aabdc75975a6e01a0932b97a960f138b
Belphegorbee <125365944+Belphegorbee@users.noreply.github.com>
2026-03-02 04:12:51 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,2 @@
-const CACHE_NAME = "papadaan-pwa-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./image/logo.png",
-  "./page/192M.png",
-  "./page/512M.png",
-  // tambahkan CSS/JS/HTML lainnya jika ada
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener('install', e => { console.log('Service Worker installed'); });
+self.addEventListener('fetch', e => { });

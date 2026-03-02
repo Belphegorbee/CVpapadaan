7c8bc83013bee2ebb019007e3e28c28ff3249915
Belphegorbee <125365944+Belphegorbee@users.noreply.github.com>
2026-03-02 22:02:46 +0800

    Update service-worker.js
    wwwww

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,46 @@
-self.addEventListener('install', e => { console.log('Service Worker installed'); });
-self.addEventListener('fetch', e => { });
+const CACHE_NAME = "cvpapadaan-v4";
+
+const urlsToCache = [
+  "/CVpapadaan/",
+  "/CVpapadaan/index.html",
+  "/CVpapadaan/manifest.json",
+  /CVpapadaan/image/logo.png,
+  "/CVpapadaan/page/192M.png",
+  "/CVpapadaan/page/512M.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log("Cache opened");
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
